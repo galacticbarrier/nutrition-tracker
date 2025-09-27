c5aceb1c49f0e66c82eca9bbfbecb0a9e4358d14
galacticbarrier <psatish@gmail.com>
2025-09-27 13:22:12 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,8 @@
-import { useState } from 'react'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div>
       <h1>Nutrition Tracker</h1>
-      <div>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+      <p>Welcome to the Nutrition Tracker app!</p>
     </div>
   )
 }
